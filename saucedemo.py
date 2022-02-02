3988c4374e48667d1271c105b5a5b2fd5818519d
chakrireddy1609 <chakrireddy1609@gmail.com>
2022-02-02 13:53:13 +0530

    tuning

--- saucedemo.py
+++ saucedemo.py
@@ -12,7 +12,7 @@ driver.get("https://www.saucedemo.com/")
 driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
-time.sleep(3)
+time.sleep(1)
 prices = driver.find_elements(By.CLASS_NAME, "inventory_item_price")
 price_list = []
 for price in prices:
